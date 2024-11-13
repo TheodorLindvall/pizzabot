f27c9c013a78f33bc3681f1b5a38b9b23bd50a1d
TheodorLindvall <theodor.lindvall@chasacademy.se>
2024-11-13 20:07:53 +0100

    Update pizzabot.js

--- pizzabot.js
+++ pizzabot.js
@@ -4,4 +4,11 @@ const pepperoni = "Pepperoni Pizza";
 
 const pizzaPrice = 80;
 
-//Put your Javscript code here:
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`);
+
+const orderName = prompt("Enter the name of the pizza you want to order today.");
+const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+const totalPrice = orderQuantity * pizzaPrice;
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${totalPrice} SEK.`);
+
